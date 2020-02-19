2043efc3b0fd3efaf5a5764a45ba216a197460c6
MeconCash <38201803+mecon-coin@users.noreply.github.com>
2020-02-19 15:59:27 +0900

    rename meconcash

--- SimpleToken.sol
+++ SimpleToken.sol
@@ -10,7 +10,7 @@ import "./ERC20Burnable.sol";
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `ERC20` functions.
  */
-contract SimpleToken is ERC20, ERC20Detailed, ERC20Burnable {
+contract MeconCashToken is ERC20, ERC20Detailed, ERC20Burnable {
     uint8 public constant DECIMALS = 8;
     uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(DECIMALS));
 
